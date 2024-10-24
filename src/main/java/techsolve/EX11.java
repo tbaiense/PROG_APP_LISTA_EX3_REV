6e014e73193d8d56d37fb004d55db70855eec9e1
tbaiense <thiago.baiense@aluno.senai.br>
2024-10-23 21:48:18 -0300

    ate 11

--- src/main/java/techsolve/EX11.java
+++ src/main/java/techsolve/EX11.java
@@ -1,5 +1,34 @@
 package techsolve;
 
+import java.util.Scanner;
+
 public class EX11 {
-    
+    public static void main(String[] args) {
+        int QTD_NUM, min, max;
+        int[] valores;
+        
+        Scanner in;
+        
+        System.out.print("Insira quantos numeros serao digitados: ");
+        in = new Scanner(System.in);
+        QTD_NUM = in.nextInt();
+        
+        valores = new int[QTD_NUM];
+        for (int i = 0; i < QTD_NUM; i++) {
+            System.out.print((i+1) + ". num.: ");
+            in = new Scanner(System.in);
+            valores[i] = in.nextInt();
+        }
+        
+        min = max = valores[0];
+        for (int n : valores) {
+            if (n < min) {
+                min = n;
+            } else if (n > max) {
+                max = n;
+            }
+        }
+        
+        System.out.print("Maior num: " +  max + "\nMenor num: " + min);
+    }
 }
